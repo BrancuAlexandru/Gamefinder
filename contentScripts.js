8f1534036e41be369d7f6902c806bc4326ba3c10
BrancuAlexandru <alexandrubrancu@yahoo.com>
2022-08-04 21:57:55 +0300

    experimented a bit, this commit has the express purpose of sharing the ideas I have

--- contentScripts.js
+++ contentScripts.js
@@ -1,7 +1,27 @@
 const videos = document.getElementsByTagName('ytd-grid-video-renderer');
+const video = videos[0];
+
+console.log(video);
+
+getVideos = async () => {
+  let link = video.getElementsByClassName('ytd-thumbnail');
+  const response = await fetch(`https://youtube.com${link}`);
+  const template = await response.text();
+  console.log(template);
+}
+
+getVideos();
+
+/*
 for (i = 0; i < videos.length; i++) {
+  (async () => {
+    const response = await fetch(`https://youtube.com/watch?v=${videos[i].data.videoId}`);
+    const template = await response.text();
+    console.log(template);
+  })
   let someText = document.createElement('p');
   someText.textContent = 'lalalala';
-  someText.style.color = '#ffffff';
+  someText.style.color = '#ababab';
   videos[i].appendChild(someText);
 }
+*/
