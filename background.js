56b0936df9151bd3c201852acc148c6c03eeef0d
BrancuAlexandru <alexandrubrancu@yahoo.com>
2022-08-17 18:29:02 +0300

    we are not getting game names along with other categories

--- background.js
+++
@@ -1,6 +0,0 @@
-/*import youtube from "@yimura/scraper";
-
-const yt = new youtube.default();
-yt.search('Never gonna give you up').then(results => {
-    console.log(results.videos[0]);
-});*/
--- contentScripts.js
+++ contentScripts.js
@@ -13,12 +13,22 @@ const checkIfVideosAreLoaded = () => {
       return allLoadedVideoIDs.filter(id => id.includes('/watch?v='));
     }
 
+    const getGameName = (html) => {
+      // only if it is in the gaming category
+      const gamingCategoryRegex = /[]/g;
+      if (html.match(gamingCategoryRegex)) {
+        const gameNameRegex = /"title":{"simpleText":(["'])(?:(?=(\\?))\2.)*?\1}/g;
+        let gameNames = [...html.matchAll(gameNameRegex)];
+        console.log(gameNames[1][0]);
+      }
+    }
+
       // Using video ID to get the HTML which includes the game title
     const getHTMLFromVideo = async (id) => {
         // note: filter out non gaming videos, append only gaming category titles to videos
       await fetch('https://www.youtube.com' + id)
       .then((response) => response.text())
-      .then((html) => console.log(html))
+      .then((html) => getGameName(html))
       .catch((err) => {
         console.warn('Something went wrong.', err);
       });
@@ -32,22 +42,20 @@ const checkIfVideosAreLoaded = () => {
       }
 
       let videoIDFilteredList = filterShortVideoIDs(allLoadedVideoIDs);
-      //for (let i = 0; i < videoIDFilteredList.length; i++) {
-        getHTMLFromVideo(videoIDFilteredList[2]);
-        // The video page has 10 - "title":{"simpleText":" - and the second one has the game name after it followed by - "}
-      //}
+      for (let i = 0; i < videoIDFilteredList.length; i++) {
+        getHTMLFromVideo(videoIDFilteredList[i]);
+      }
     }
     getVideoIDs();
     clearMyInterval();
   }
 }
 
-const myInterval = setInterval(checkIfVideosAreLoaded, 500);
+const myInterval = setInterval(checkIfVideosAreLoaded, 100);
 
-/* 
+/*
   - Get index of removed IDs from videoIDFilteredList, and remove those at the same index from allLoadedVideoIDs
   - Get all /["title":{"simpleText":"..."}]/ from the HTML, then put them in an array, then remove all but the 2nd
   - Append the name indexed in videoIDFilteredList to a video with the same index as a <p>
-  - Modify the timeout method from setTimeout to a check every 500ms if something inside the video element exists
   - Detect if new videos are loaded, if so run the main function again, but only for the newly loaded videos (maybe scroll position)
 */
