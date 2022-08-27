ab48d4a64027a1215810da9a1f8775af217e33cc
BrancuAlexandru <alexandrubrancu@yahoo.com>
2022-08-27 17:07:46 +0300

    update, getting half the data needed to match videos and game names

--- contentScripts.js
+++ contentScripts.js
@@ -8,16 +8,36 @@ const clearMyInterval = () => {
 
 const checkIfVideosAreLoaded = () => {
   if (videosAreLoaded) {
+    const compareGameIdToVideos = (data) => {
+      let games = [];
+      let elements = [];
+      for (let i = 0; i < 5; i++) {
+        elements.push([data.videoIDs[i], data.videos[i]]);
+        games.push([data.gameVideoIDs[i], data.gameNames[i]]);
+        //console.log(games[i]);
+        /*for (let j = 0; j < videos.length; j++) {
+          if (games[i][0] === elements[j][0]) {
+            let paragraph = document.createElement('p');
+            paragraph.textContent = games[i][1];
+            paragraph.style.color = '#000000';
+            elements[j][1].parentElement.parentElement.appendChild(paragraph); // .parentElement isn't working
+          }
+        }*/
+      }
+    }
+
     const getGameName = (html) => {
       const gameNameRegex = /(?<="title":{"simpleText":)(["'])(?:(?=(\\?))\2.)*?\1/g;
         // Regular expression breakdown:
         // (?<="title":{"simpleText":) everything after "title":{"simpleText":
         // (["']) everything in quotes up to \1 (end quote)
         // (?:(?=(\\?))\2.)*? ingore backslashes
-      let gameNames = [...html.matchAll(gameNameRegex)][1][0];
-      if (gameNames.includes("Mix - ") || gameNames.includes("You may also like...")) {
-        gameNames = null;
+      let gameName = [...html.matchAll(gameNameRegex)][1][0];
+      let result;
+      if (!gameName.includes("Mix - ") && !gameName.includes("You may also like...")) {
+        result = gameName;
       }
+      return result;
     }
 
       // Using video ID to get the HTML which includes the game title
@@ -32,29 +52,44 @@ const checkIfVideosAreLoaded = () => {
         // (?<="category":) everything after "category":
         // (["']) everything in quotes up to \1 (end quote)
         // (?:(?=(\\?))\2.)*? ingore backslashes
+      let result = [];
       if (categoryRegex[0] === `"Gaming"`) {
-        getGameName(html);
+        result[0] = getGameName(html);
+        result[1] = id;
       }
+      return result;
     }
 
-    const getVideoIDs = () => {
-      let allLoadedVideoTitles= [document.querySelectorAll("#video-title")][0];
+      // Filtering out the shorts from the ID list
+    const filterShortVideoIDs = (allLoadedVideoIDs) => {
+      return allLoadedVideoIDs.filter(id => id.includes('/watch?v='));
+    }
+
+    const getVideoIDs = async () => {
+      let allLoadedVideoTitles = [...document.querySelectorAll("#video-title")];
       let allLoadedVideoIDs = [];
+      let videos = [];
       for (let e of allLoadedVideoTitles) {
         allLoadedVideoIDs.push(e.getAttribute('href'));
+        if (e.getAttribute('href').includes('/watch?v=')) {
+          videos.push(allLoadedVideoTitles[allLoadedVideoTitles.indexOf(e)]);
+        }
       }
-
-        // Filtering out the shorts from the ID list
-      const filterShortVideoIDs = (allLoadedVideoIDs) => {
-        return allLoadedVideoIDs.filter(id => id.includes('/watch?v='));
-      }
+      let result = {};
+      result.videos = videos;
 
       let videoIDFilteredList = filterShortVideoIDs(allLoadedVideoIDs);
+      result.videoIDs = videoIDFilteredList;
+
       for (let i = 0; i < videoIDFilteredList.length; i++) {
-        getHTMLFromVideo(videoIDFilteredList[i]);
+        let response = getHTMLFromVideo(videoIDFilteredList[i]);
+        result.gameNames = response[0];
+        result.gameVideoIDs = response[1];
       }
+      return result;
     }
-    getVideoIDs();
+    getVideoIDs().then((data) => console.log(data));
+    //compareGameIdToVideos(data);
     clearMyInterval();
   }
 }
@@ -62,8 +97,5 @@ const checkIfVideosAreLoaded = () => {
 const myInterval = setInterval(checkIfVideosAreLoaded, 300);
 
 /*
-  - Get index of removed IDs from videoIDFilteredList, and remove those at the same index from allLoadedVideoIDs
-  - Get all /["title":{"simpleText":"..."}]/ from the HTML, then put them in an array, then remove all but the 2nd
-  - Append the name indexed in videoIDFilteredList to a video with the same index as a <p>
   - Detect if new videos are loaded, if so run the main function again, but only for the newly loaded videos (maybe scroll position)
 */
