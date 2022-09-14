18ec6222d0f51a06cc1cfbf32ee971913b445b24
BrancuAlexandru <alexandrubrancu@yahoo.com>
2022-09-14 20:58:21 +0300

    minor update

--- scriptLoader.js
+++ scriptLoader.js
@@ -6,6 +6,7 @@ if (
   initialURL === "https://www.youtube.com/feed/explore" ||
   initialURL === "https://www.youtube.com/gaming" ||
   initialURL.match(/https\:\/\/www\.youtube\.com\/c\/.*\/videos/g) ||
+  initialURL.match(/https\:\/\/www\.youtube\.com\/user\/.*\/videos/g) ||
   initialURL.includes("https://www.youtube.com/feed/trending")
   ) {
 
@@ -45,55 +46,6 @@ if (
   script.onload = function() {
     this.remove();
   };
-}
-
-const scriptLoader = (url) => {
-  if (
-    url === "https://www.youtube.com/feed/subscriptions" ||
-    url === "https://www.youtube.com/feed/explore" ||
-    url === "https://www.youtube.com/gaming" ||
-    url.match(/https\:\/\/www\.youtube\.com\/c\/.*\/videos/g) ||
-    url.includes("https://www.youtube.com/feed/trending")
-    ) {
-
-    if (document.getElementById('gamefinderExtensionScript')) {
-      document.getElementById('gamefinderExtensionScript').remove();
-    };
-    let script = document.createElement('script');
-    script.id = 'gamefinderExtensionScript';
-    script.src = chrome.runtime.getURL("subscriptions.js");
-    (document.head || document.documentElement).appendChild(script);
-    script.onload = function() {
-      this.remove();
-    };
-  } else if (
-    url === "https://www.youtube.com/" ||
-    url.includes("https://www.youtube.com/channel/")
-    ) {
-
-    if (document.getElementById('gamefinderExtensionScript')) {
-      document.getElementById('gamefinderExtensionScript').remove();
-    };
-    let script = document.createElement('script');
-    script.id = 'gamefinderExtensionScript';
-    script.src = chrome.runtime.getURL("home.js");
-    (document.head || document.documentElement).appendChild(script);
-    script.onload = function() {
-      this.remove();
-    };
-  } else if (url.includes("https://www.youtube.com/watch?v=")) {
-    
-    if (document.getElementById('gamefinderExtensionScript')) {
-      document.getElementById('gamefinderExtensionScript').remove();
-    };
-    let script = document.createElement('script');
-    script.id = 'gamefinderExtensionScript';
-    script.src = chrome.runtime.getURL("recommended.js");
-    (document.head || document.documentElement).appendChild(script);
-    script.onload = function() {
-      this.remove();
-    };
-  }
 }
 
 const URLChangeChecker = () => {
